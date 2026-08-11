307be24dd01bc2c26a0feae6297cb9009005aa69
addehvn <addehoviannem@gmail.com>
2026-08-11 17:57:22 +0400

    Add single room route by Hotel and room ID

--- routes/rooms.js
+++ routes/rooms.js
@@ -29,6 +29,32 @@ router.get('/hotel/:hotelId/allRooms',(req,res,next)=>{
   });
 });
 
+router.get('/hotel/:hotelId/room/:roomId',(req,res,next)=>{
+  const hotel_id=req.params.hotelId;
+  const room_id=req.params.roomId;
 
+  const sql=`
+  SELECT 
+    room_type,
+    price,
+    capacity,
+    image 
+  FROM rooms 
+  WHERE hotel_id=?AND room_id=?;
+  `;
+
+  db.query(sql,[hotel_id,room_id],(err,result)=>{
+    if(err){
+      return next(err);
+    };
+    if(result.length===0){
+      const error = new Error("hotel doesn't  exists or doesn't have free rooms");
+      error.status=404;
+      return next(error);
+    };
+    res.status(200).send(result);
+  });
+
+});
 
 module.exports=router
